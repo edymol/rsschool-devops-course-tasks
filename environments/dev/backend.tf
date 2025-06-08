be435560bada64067a7a86228901e4dee7beedc2
edymol <edymolinap@gmail.com>
2025-06-08 22:49:16 +0100

    add: backend.tf for tfstate files

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,11 +1,10 @@
-# # environments/dev/backend.tf
-#
-# terraform {
-#   backend "s3" {
-#     bucket = "rsschool-devops-tfstate-dev"
-#     key    = "dev/terraform.tfstate"
-#     # The region must be hardcoded in the backend configuration.
-#     region = "eu-west-1"
-#     encrypt = true
-#   }
-# }
+# environments/dev/backend.tf
+
+terraform {
+  backend "s3" {
+    bucket = "rsschool-devops-tfstate-dev"
+    key    = "dev/terraform.tfstate"
+    region = "eu-west-1"
+    encrypt = true
+  }
+}
