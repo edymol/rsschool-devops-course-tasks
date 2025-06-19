a1076c56df7c43e0ee05280eb2b0526fd437faac
edymol <edymolinap@gmail.com>
2025-06-19 23:25:20 +0100

    add: ssh from anywhere

--- terraform/vpc/security_groups.tf
+++ terraform/vpc/security_groups.tf
@@ -8,6 +8,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
     # cidr_blocks = [var.my_ip]
     description = "SSH access to bastion from trusted IP"
   }
